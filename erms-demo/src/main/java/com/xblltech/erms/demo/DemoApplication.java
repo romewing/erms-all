ca452a91b7cc396bcbbb639d0859292631987b25
zhangyi <zhangyi@xblltech.cn>
2017-11-01 18:30:43 +0800

    Initial commit

--- erms-demo/src/main/java/com/xblltech/erms/demo/DemoApplication.java
+++ erms-demo/src/main/java/com/xblltech/erms/demo/DemoApplication.java
@@ -1,14 +1,27 @@
 package com.xblltech.erms.demo;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @RestController
+@EnableSwagger2
+
+@Api
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -17,7 +30,27 @@ public class DemoApplication {
 
     @RequestMapping("/demo")
     @GetMapping
+    @ApiOperation(value = "这是Value值")
     public String demo() {
         return "hello demo";
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xblltech"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
+                .termsOfServiceUrl("http://blog.didispace.com/")
+                .contact("程序猿DD")
+                .version("1.0")
+                .build();
     }
 }
---
+++ erms-demo/src/main/java/com/xblltech/erms/demo/ResourceServerConfiguration.java
@@ -0,0 +1,8 @@
+package com.xblltech.erms.demo;
+
+
+/**
+ * @author ZHANG Yi
+ */
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter{
+}
