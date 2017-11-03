d9c32d580c12e271aaf2e6656cca7281e2e249af
zhangyi <zhangyi@xblltech.cn>
2017-11-03 18:12:34 +0800

    Initial commit

--- erms-auth/src/main/java/com/xblltech/erms/ResourceServerConfigurer.java
+++ erms-auth/src/main/java/com/xblltech/erms/ResourceServerConfigurer.java
@@ -1,4 +1,12 @@
+
+
+
 package com.xblltech.erms;
 
-public class ResourceServerConfigurer {
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * @author ZHANG Yi
+ */
+public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter{
 }
