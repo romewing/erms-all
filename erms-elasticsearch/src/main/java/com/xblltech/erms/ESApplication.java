21e62f60e266e144bfd153fb262b119af3851c28
zhangyi <zhangyi@xblltech.cn>
2017-11-24 18:21:31 +0800

    Initial commit

--- erms-elasticsearch/src/main/java/com/xblltech/erms/ESApplication.java
+++ erms-elasticsearch/src/main/java/com/xblltech/erms/ESApplication.java
@@ -1,9 +1,16 @@
 package com.xblltech.erms;
 
 
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.document.DocumentField;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +20,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableKafka
@@ -20,6 +28,14 @@ public class ESApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(ESApplication.class);
+        TransportClient transportClient = context.getBean(TransportClient.class);
+        SearchResponse searchResponse = transportClient.prepareSearch("bid").setTypes("sprecc").setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.termQuery("name", "四川"))                 // Query
+                .setFrom(0).setSize(60).setExplain(true)
+                .get();
+        System.out.println(searchResponse.getHits().totalHits+"===");
+        for (SearchHit hit : searchResponse.getHits().getHits()) {
+        }
 
     }
 
